034fa4e27c8bdb1c57ea14200e435a62bc0a9ff4
Lencroix <124858206+Lencroix@users.noreply.github.com>
2024-02-08 05:52:03 -0500

    Header

--- WEB24-01/src/components/Card/card.jsx
+++ WEB24-01/src/components/Card/card.jsx
@@ -19,12 +19,7 @@ export function Card(){
                 <h2 className='card-title'>Card Title</h2>
                  <p className='card-description'>This is a card description you can add more info about it here</p>
             </div>
-            <div className='card-container'>
-                 <img src="https://picsum.photos/300/200" alt="" className="card-img" />
-                <h2 className='card-title'>Card Title</h2>
-                 <p className='card-description'>This is a card description you can add more info about it here</p>
-            </div>
-            
+           
             
         </div>
         )
--- WEB24-01/src/components/Header/header.jsx
+++ WEB24-01/src/components/Header/header.jsx
@@ -3,16 +3,30 @@ import React from 'react'
 export function Header(){
         return(
         <header>
-            <div class="navbar">
+            <nav class="navbar">
                 <img class="menu" src="../public/menu.png" alt="" />
                 <img class="logo" src="../public/logoweb.png" alt="" />
                 <img class="profile" src="../public/profile.png" alt="" />
+                <div className='header-text'>
+           
+                <div class="dropdown">
+                    <p class="about-us" tabindex="0">ABOUT US ▼</p>
+                        <div class="dropdown-content">
+                            <ul>
+                                 <li><a href="#">OUR STORY</a></li>
+                                 <li><a href="#">LOCATIONS</a></li>
+                                <li><a href="#">MANUFACTURES</a></li>
+                            </ul>
+                        </div>
+                </div>
+
                 <div class="text">
                     <button class="btn-secondary">
                         CREATE ACCOUNT
                  </button>
+                 </div>
                 </div>
-            </div>
+            </nav>
         </header>
         )
       
